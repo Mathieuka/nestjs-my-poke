a557dc2a327b37326caf69abee456fcf972ae9d9
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-01-31 16:22:44 +0100

    chore(main.ts): remove commented out code
    - Remove commented out import of `AppModule`
    - Remove commented out instantiation of `AppModule` in `NestFactory.create()`

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
 import { PokemonModule } from './infrastructure/modules/pokemon.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create(PokemonModule);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
