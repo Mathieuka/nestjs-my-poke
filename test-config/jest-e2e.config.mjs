0f786806d4d96d12ee0a434f2e54afa87425a3fd
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-01 14:02:38 +0100

    chore(test): remove test from Controller

--- test-config/jest-e2e.config.mjs
+++ test-config/jest-e2e.config.mjs
@@ -5,7 +5,7 @@ const config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '../',
   testEnvironment: 'node',
-  testRegex: ['.*\\e2e.spec\\.ts$'],
+  testRegex: ['.*\\.spec\\.ts$'],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
