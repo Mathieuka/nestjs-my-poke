cdb6c4171d28840b4bc26b67810daa20fc465a37
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-22 19:33:48 +0100

    chore(infrastructure): remove option dropSchema and synchronize

--- src/infrastructure/seeding/index.ts
+++ src/infrastructure/seeding/index.ts
@@ -15,10 +15,8 @@ const options: DataSourceOptions & SeederOptions = {
   password: 'password',
   database: 'my-pokemon-dev',
   entities: [Pokemon, User],
-  // migrationsRun: false,
+  migrationsRun: false,
   seedTracking: false,
-  dropSchema: true,
-  synchronize: true,
   factories: [factoryPath],
   seeds: [seedPath],
 };
