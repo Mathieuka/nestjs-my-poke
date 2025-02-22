c07e3910fe5ab02cf72c9a37ee2912bd3e8d50f9
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-22 19:47:09 +0100

    feat(pokemon.controller): add parameter to getPokemon method
    - Add parameter 'id' to getPokemon method in PokemonController
    - Update route to '/pokemon/:id' to accept id parameter

--- src/infrastructure/controllers/pokemon/pokemon.controller.ts
+++ src/infrastructure/controllers/pokemon/pokemon.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { GetPokemonUseCase } from '@/application/usecases';
 
 @Controller()
 export class PokemonController {
   constructor(private readonly getPokemonUseCase: GetPokemonUseCase) {}
 
-  @Get()
-  getPokemon(): any {
+  @Get('/pokemon/:id')
+  getPokemon(@Param('id') id: string): any {
+    // console.log('%c LOG id', 'background: #222; color: #bada55', id);
     return this.getPokemonUseCase.execute();
   }
 }
