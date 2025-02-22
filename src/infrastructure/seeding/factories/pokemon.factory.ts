9d45a899da030f9e2f267fdfc005981e9e135073
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-22 19:01:44 +0100

    fix(infrastructure): update pokemon type in pokemon factory

--- src/infrastructure/seeding/factories/pokemon.factory.ts
+++ src/infrastructure/seeding/factories/pokemon.factory.ts
@@ -4,6 +4,6 @@ import { Pokemon } from '@/infrastructure/database/entities/pokemon.entity';
 export default setSeederFactory(Pokemon, (faker) => {
   const pokemon = new Pokemon();
 
-  pokemon.type = 'Water';
+  pokemon.type = 'Pikachu';
   return pokemon;
 });
