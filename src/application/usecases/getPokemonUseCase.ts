136a6a21bb6962a1aa7cc65aac4e130df13a989b
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-22 17:22:23 +0100

    refactor(database): refactor repository structure
    - Move `PokemonRepository` from `database` folder to `repositories` folder
    - Update import paths in affected files

--- src/application/usecases/getPokemonUseCase.ts
+++ src/application/usecases/getPokemonUseCase.ts
@@ -1,6 +1,6 @@
 import { IPokemon } from '@/core/domain/entities/pokemon';
 import { Injectable } from '@nestjs/common';
-import { PokemonRepository } from '@/infrastructure/database/repositories/pokemon/pokemonRepository';
+import { PokemonRepository } from '@/infrastructure/repositories/pokemon/pokemonRepository';
 
 @Injectable()
 export class GetPokemonUseCase {
--- src/modules/pokemon.module.ts
+++ src/modules/pokemon.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { GetPokemonUseCase } from '@/application/usecases';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pokemon } from '@/infrastructure/database/entities/pokemon.entity';
-import { PokemonRepository } from '@/infrastructure/database/repositories/pokemon/pokemonRepository';
 import { DatabaseModule } from '@/modules/database.module';
 import { PokemonController } from '@/infrastructure/controllers';
+import { PokemonRepository } from '@/infrastructure/repositories/pokemon/pokemonRepository';
 
 @Module({
   imports: [DatabaseModule, TypeOrmModule.forFeature([Pokemon])],
