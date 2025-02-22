48d1fe75ae4b69a8e345b6879252ac8971bf66f3
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-02-22 17:13:59 +0100

    feat(seeding): update user factory to generate random user data
    - Update user name to be a random first name using faker library
    - Add generation of random email using faker library

--- src/infrastructure/seeding/factories/user.factory.ts
+++ src/infrastructure/seeding/factories/user.factory.ts
@@ -4,8 +4,9 @@ import { User } from '@/infrastructure/database/entities/user.entity';
 export default setSeederFactory(User, (faker) => {
   const user = new User();
 
-  user.name = 'Matt';
-
+  user.name = faker.person.firstName();
   user.password = faker.internet.password();
+  user.email = faker.internet.email();
+
   return user;
 });
